ce2cc26597a959ced20b5b4fe3f5f589ca37dd94
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2020-08-27 16:18:26 -0300

    VOLUME de uma lata de óleo
    Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula VOLUME <- 3.14159 * R ^ 2 * ALTURA.

---
+++ Volume lata de Oleo/Volume lata de Oleo/Volume lata de Oleo.cpp
@@ -0,0 +1,16 @@
+// Volume lata de Oleo.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+int main()
+{
+    double altura, volume, R;
+    std::cout << "Informe a altura: ";
+    std::cin >> altura;
+    std::cout << "Informe o valor de R: ";
+    std::cin >> R;
+    volume = 3.14159 * pow(R, 2) * altura;
+    std::cout << "O valor do VOLUME = : " << volume <<"\n";
+
+}
