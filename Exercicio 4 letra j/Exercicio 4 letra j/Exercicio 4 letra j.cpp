601c9ee566001fd92ac0b7c8681e192b0d8421d7
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2020-08-28 00:07:15 -0300

    Diferença de A e B
    Exercício 4 letra j. Do livro ALGORITMOS - Lógica para desenvolvimento de programação de computadores. 29º Edição.
    José Augusto N. G. Manzano e Jayr Figueiredo de Oliveira
    Ler dois valores numéricos inteiros (representados pelas variáveis A e B) e apresentar o resultado armazenado em memória do quadrado da  
    diferença do primeiro valor (variável A) em relação ao segundo valor (variável B).

---
+++ Exercicio 4 letra j/Exercicio 4 letra j/Exercicio 4 letra j.cpp
@@ -0,0 +1,20 @@
+//Exercicio 4 letra j.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//Exercicio 4 letra e. Do livro ALGORITMOS - Lógica para desenvolvimento de programação de computadores. 29º Edição.
+//José Augusto N. G. Manzano e Jayr Figueiredo de Oliveira
+//Ler dois valores numéricos inteiros (representados pelas variáveis A e B) e apresentar o resultado armazenado em memória do quadrado da  
+//diferença do primeiro valor (variável A) em relação ao segundo valor (variável B). 
+
+#include <iostream>
+
+int main()
+{
+    int A, B, DIF;
+    std::cout << "\nDigite o primeiro valor: ";
+    std::cin >> A;
+    std::cout << "\nDigite o segundo valor: ";
+    std::cin >> B;
+
+    DIF = A - B;
+
+    std::cout << "\nDiferenca = " << DIF <<"\n\n";
+}
