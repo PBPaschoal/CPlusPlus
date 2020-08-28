ac200e67fdabd884ea7511082fa7592dc331f128
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2020-08-28 00:08:39 -0300

    Update Exercicio 4 letra h.cpp

--- Exercicio 4 letra h/Exercicio 4 letra h/Exercicio 4 letra h.cpp
+++ Exercicio 4 letra h/Exercicio 4 letra h/Exercicio 4 letra h.cpp
@@ -1,5 +1,5 @@
 //Exercicio 4 letra h.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//Exercicio 4 letra e. Do livro ALGORITMOS - Lógica para desenvolvimento de programação de computadores. 29º Edição.
+//Exercicio 4 letra h. Do livro ALGORITMOS - Lógica para desenvolvimento de programação de computadores. 29º Edição.
 //José Augusto N. G. Manzano e Jayr Figueiredo de Oliveira
 //Elaborar um programa que calcule e apresente o valor do volume de uma caixa retangular, utilizando a fórmula
 //VOLUME <- COMPRIMENTO * LARGURA * ALTURA
