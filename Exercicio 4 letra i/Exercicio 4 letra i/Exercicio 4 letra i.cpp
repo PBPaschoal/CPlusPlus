c6081d8e880cfe194a73f67598d44bf1466b6cb6
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2020-08-28 00:08:04 -0300

    Update Exercicio 4 letra i.cpp

--- Exercicio 4 letra i/Exercicio 4 letra i/Exercicio 4 letra i.cpp
+++ Exercicio 4 letra i/Exercicio 4 letra i/Exercicio 4 letra i.cpp
@@ -1,5 +1,5 @@
 //Exercicio 4 letra i.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//Exercicio 4 letra e. Do livro ALGORITMOS - Lógica para desenvolvimento de programação de computadores. 29º Edição.
+//Exercicio 4 letra i. Do livro ALGORITMOS - Lógica para desenvolvimento de programação de computadores. 29º Edição.
 //José Augusto N. G. Manzano e Jayr Figueiredo de Oliveira
 //Efetuar a leitura de um valor numérico inteiro e apresentar o resultado do valor lido elevado ao quadrado, sem efetuar o armazenamento 
 //do resultado em memória.
